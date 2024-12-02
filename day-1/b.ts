603934ebdcb6caf490ace442300810bd009dda4e
dakotachatt <dvchatt@gmail.com>
2024-12-02 06:56:40 -0700

    day 1b

--- day-1/b.ts
+++ day-1/b.ts
@@ -2,8 +2,36 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day1b(data: string[]) {
-  console.log(data);
-  return 0;
+  const left = [];
+  const right = [];
+
+  let sumOfMultiples = 0;
+
+  // Put each column into into it's own array
+  for(const line of data) {
+    const splitLine = line.split('   ');
+
+    left.push(splitLine[0]);
+    right.push(splitLine[1]);
+  }
+
+  left.sort();
+  right.sort();
+
+  for(let i = 0; i < left.length; i++) {
+    let count = 0;
+
+    for(let j = 0; j < right.length; j++) {
+      if(left[i] === right[j]) {
+        count++;
+      }
+    }
+
+    sumOfMultiples += left[i] * count;
+    count = 0;
+  }
+
+  return sumOfMultiples;
 }
 
 await runSolution(day1b);
