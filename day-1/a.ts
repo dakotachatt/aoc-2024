c34f3e5484bc439312fdfea3dcb7e8015fbf80a9
dakotachatt <dvchatt@gmail.com>
2024-12-01 19:18:44 -0700

    day 1 part 1

--- day-1/a.ts
+++ day-1/a.ts
@@ -2,8 +2,27 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day1a(data: string[]) {
-  console.log(data);
-  return 0;
+  const left = [];
+  const right = [];
+
+  let sumOfDifferences = 0;
+
+  // Put each column into into it's own array
+  for(const line of data) {
+    const splitLine = line.split('   ');
+
+    left.push(splitLine[0]);
+    right.push(splitLine[1]);
+  }
+
+  left.sort();
+  right.sort();
+
+  for(let i = 0; i < left.length; i++) {
+    sumOfDifferences += Math.abs(right[i] - left[i]);
+  }
+
+  return sumOfDifferences;
 }
 
 await runSolution(day1a);
