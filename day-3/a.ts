52175bcb67cc51e3075d02f8018d881f9ca47fce
dakotachatt <dvchatt@gmail.com>
2024-12-03 19:35:25 -0700

    day 3 part a

--- day-3/a.ts
+++ day-3/a.ts
@@ -2,8 +2,24 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
-  console.log(data);
-  return 0;
+  let result = 0;
+
+  for(let i = 0; i < data.length; i++) {
+    const matchArray = data[i].match(/mul\(\d+,\d+\)/g);
+
+    console.log(matchArray);
+
+    for(let j = 0; j < matchArray.length; j++) {
+      const match = matchArray[j];
+      const numberArray = match.replaceAll('mul(', '').replaceAll(')', '').split(',').map(Number);
+
+      console.log(numberArray);
+
+      result += (numberArray[0] * numberArray[1]);
+    }
+  }
+
+  return result;
 }
 
 await runSolution(day3a);
